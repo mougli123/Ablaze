5b725d2682121b9a11eef06be71c112a5b57edb6
Carl Dave <paikerchu13@gmail.com>
2017-02-15 17:12:37 -0500

    Game

---
+++ Assets/level.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class level : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    foreach (GameObject j in GameObject.FindGameObjectsWithTag("Block")) {
+            j.transform.position = new Vector2(Mathf.RoundToInt(j.transform.position.x), Mathf.RoundToInt(j.transform.position.y));
+            j.gameObject.transform.parent = this.transform;
+        }
+	}
+}
