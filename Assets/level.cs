a222c669aa8bfcafce964bacf26aec9e6695ff47
Carl Dave <paikerchu13@gmail.com>
2017-04-01 19:13:58 -0400

    door put in game

--- Assets/level.cs
+++ Assets/level.cs
@@ -15,6 +15,7 @@ public class level : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if (!Application.isPlaying) {
+            transform.position = new Vector2(0, 0);
             foreach (GameObject j in GameObject.FindGameObjectsWithTag("Block")) {
                 j.transform.position = new Vector2(Mathf.RoundToInt(j.transform.position.x), Mathf.RoundToInt(j.transform.position.y));
                 j.gameObject.transform.parent = this.transform;
